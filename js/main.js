f05ef3323d5f1e8f81ec21780ae74efdbedfe504
Serhiy Hulyi <serhiyhulyi93@gmail.com>
2015-12-27 20:55:57 +0200

    е

--- js/main.js
+++ js/main.js
@@ -89,19 +89,23 @@ jQuery(document).ready(function($) {
     $( "#infra_tabs" ).tabs();
   });
 
-   var $status = $('.pagingInfo');
-   var $slickElement = $('.sliderChoose');
+  var $status = $('.pagingInfo');
+  var $slickElement = $('.sliderChoose');
 
-    $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-        //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
-        var i = (currentSlide ? currentSlide : 0) + 1;
-        $status.text(i + '/' + slick.slideCount);
-    });
-
+  $(function(){
     $slickElement.slick({
         autoplay: false,
         dots: true
     });
+  })
+
+  $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+      //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+      var i = (currentSlide ? currentSlide : 0) + 1;
+      $status.text(i + '/' + slick.slideCount);
+  });
+
+    
 
 
 
