2db01e329671c1899483b915b4f6fa67133f005c
OlehRusyi <olehrusy@gmail,com>
2015-12-28 11:53:53 +0200

    ів

--- js/main.js
+++ js/main.js
@@ -89,19 +89,6 @@ jQuery(document).ready(function($) {
     $( "#infra_tabs" ).tabs();
   });
 
-   var $status = $('.pagingInfo');
-   var $slickElement = $('.sliderChoose');
-
-    $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-        //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
-        var i = (currentSlide ? currentSlide : 0) + 1;
-        $status.text(i + '/' + slick.slideCount);
-    });
-
-    $slickElement.slick({
-        autoplay: false,
-        dots: true
-    });
 
 
 
@@ -458,5 +445,17 @@ jQuery(document).ready(function($) {
 
 });
 
+    var $status = $('.pagingInfo');
+    var $slickElement = $('.sliderChoose');
+
+    $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+        //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+        var i = (currentSlide ? currentSlide : 0) + 1;
+        $status.text(i + '/' + slick.slideCount);
+    });
+
+    $slickElement.slick({
+        autoplay: true
+    });
 
 
